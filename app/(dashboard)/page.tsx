819cfa1e66c0d19cb4ab348306682efde9065458
Tayrika <rika13tay@gmail.com>
2024-05-15 15:44:45 +0900

    Sign-in and Sign-out authentication with Clerk

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-    <p>
-      This is an authenticated route.
-    </p>
+    <UserButton afterSignOutUrl="/" />
   )
 }
 
